696bfcc62b8c825aca5e3ca2263f8d1d769853bc
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-11 11:22:07 +0100

    :memo: pages added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
 import "@fontsource/readex-pro";
 import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-import type { NextPage } from "next";
+import styled from "styled-components";
+import AnimeList from "../components/AnimeList/AnimeList";
 
-const Home: NextPage = () => {
-  return <></>;
+const ContainerMain = styled.section`
+  min-height: 92.2vh;
+  margin-top: 60px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  background-color: #000;
+`;
+
+const Home = (): JSX.Element => {
+  return (
+    <ContainerMain>
+      <AnimeList />
+    </ContainerMain>
+  );
 };
 
 export default Home;
