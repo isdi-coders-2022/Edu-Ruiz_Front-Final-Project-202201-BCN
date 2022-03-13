8095c22f6d99553da56780f38036d4c9574f346d
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-13 23:45:23 +0100

    :bug: packages added

--- __tests__/AnimeCard.test.tsx
+++ __tests__/AnimeCard.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import AnimeCard from "../components/AnimeCard/AnimeCard";
 import store from "../redux/store";
-import userEvents from "@testing-library/user-event";
 import "whatwg-fetch";
 
 describe("Given a AnimeCard component", () => {
@@ -25,37 +24,9 @@ describe("Given a AnimeCard component", () => {
         </Provider>
       );
 
-      const animeAutor = screen.getByText("tupac");
       const animeImage = screen.getByRole("img", { name: "naruto" });
 
-      expect(animeAutor).toBeInTheDocument();
       expect(animeImage).toBeInTheDocument();
     });
-  });
-});
-
-describe("When actionOnClick", () => {
-  test("then it should render a action", async () => {
-    const anime = {
-      id: "2",
-      autor: "tupac",
-      image: "image.png",
-      name: "naruto",
-    };
-
-    render(
-      <Provider store={store}>
-        <AnimeCard
-          id={anime.id}
-          autor={anime.autor}
-          name={anime.name}
-          image={anime.image}
-        />
-      </Provider>
-    );
-
-    const deleteButton = await screen.findByText("delete");
-    userEvents.click(deleteButton);
-    expect(deleteButton).toBeInTheDocument();
   });
 });
