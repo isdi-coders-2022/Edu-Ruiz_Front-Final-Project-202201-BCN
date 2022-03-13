970572c72f0d279a4e744556ee58c60251a308fb
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-13 23:41:58 +0100

    :bug: new anime added

--- pages/new-anime.tsx
+++ pages/new-anime.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+import NewAnime from "../components/newAnime/newAnime";
+
+const ContNewAnime = styled.section`
+  min-height: 100vh;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #1a0e17;
+`;
+
 const newAnime = () => {
-  return <></>;
+  return (
+    <>
+      <ContNewAnime>
+        <NewAnime />
+      </ContNewAnime>
+    </>
+  );
 };
 
 export default newAnime;
