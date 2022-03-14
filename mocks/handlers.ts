720a1c3cb554ad192c6ad64bfbcc36c61d62c6f3
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-14 14:58:39 +0100

    :clown_face: create mock added

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -28,4 +28,18 @@ export const handlers = [
   rest.delete(`${process.env.NEXT_PUBLIC_ANIME4ME}animes/10`, (req, res, ctx) =>
     res(ctx.status(404), ctx.json({}))
   ),
+  rest.post(
+    `${process.env.NEXT_PUBLIC_ANIME4ME}animes/create`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(201),
+        ctx.json({
+          id: "2",
+          autor: "tupac",
+          image: "image.png",
+          name: "naruto",
+        })
+      );
+    }
+  ),
 ];
