6975ea238fd072e798df1cb2d05256bc3fcbed9e
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-18 12:35:11 +0100

    :zap: redux addef

--- redux/reducer/index.ts
+++ redux/reducer/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import animeReducer from "./animeReducer";
+import singleAnimeReducer from "./singleReducer";
 
 const rootReducer = combineReducers({
   animes: animeReducer,
+  anime: singleAnimeReducer,
 });
 
 export default rootReducer;
