e83d18551123f218e50547b79b34d60d8fe71614
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-21 03:55:12 +0100

    :fire: pwa added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
 const nextConfig = {
   compiler: { styledComponents: true },
   reactStrictMode: true,
   images: {
     domains: ["api-cdn.myanimelist.net", "cdn.myanimelist.net"],
   },
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
