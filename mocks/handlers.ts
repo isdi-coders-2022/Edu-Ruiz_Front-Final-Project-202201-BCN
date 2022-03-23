2267af41db8ca083b56c1a2912ac463eadd0aa22
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-23 18:47:36 +0100

    :clown_face: mocks added

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -42,10 +42,69 @@ export const handlers = [
       );
     }
   ),
+  rest.patch(
+    `${process.env.NEXT_PUBLIC_ANIME4ME}animes/622cdb2eaa2f5a4e7dd16917`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(201),
+        ctx.json({
+          id: "622cdb2eaa2f5a4e7dd16917",
+          autor: "tupac",
+          image: "image.png",
+          name: "naruto",
+        })
+      );
+    }
+  ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_ANIME4ME}animes/622cdb2eaa2f5a4e7dd16917`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(201),
+        ctx.json({
+          id: "622cdb2eaa2f5a4e7dd16917",
+          autor: "tupac",
+          image: "image.png",
+          name: "naruto",
+        })
+      );
+    }
+  ),
   rest.post(
     `${process.env.NEXT_PUBLIC_ANIME4ME}animes/create`,
     (req, res, ctx) => {
       return res(ctx.status(400), ctx.json({}));
     }
+  ),
+  rest.post(
+    `${process.env.NEXT_PUBLIC_ANIME4ME}users/login`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({ username: "uwu", password: "uwu" })
+      );
+    }
+  ),
+  rest.post(
+    `${process.env.NEXT_PUBLIC_ANIME4ME}users/register`,
+    (req, res, ctx) => res(ctx.status(200))
+  ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_ANIME4ME}users/allusers`,
+    (req, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json({
+          users: [{ username: "uwu", password: "uwu", image: "image.png" }],
+        })
+      )
+  ),
+  rest.get(`${process.env.NEXT_PUBLIC_ANIME4ME}users/user`, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        user: { username: "uwu", password: "uwu", image: "image.png" },
+      })
+    )
   ),
 ];
